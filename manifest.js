b7e1c5077f6901b6386a88c0259da284f9ead1f5
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-05-20 17:11:52 +0200

    Update plugin

--- manifest.js
+++ manifest.js
@@ -4,4 +4,11 @@ module.exports = {
         pluginContext.registerPolicy(require('./policies/auth0policy.js'));
     },
     policies:['auth0'],
+    schema: {  // This is for CLI to ask about params 'eg plugin configure example'
+        baseUrl: {
+            title: 'param',
+            description: 'param',
+            type: 'string',
+        },
+    }
 };
--- policies/auth0policy.js
+++ policies/auth0policy.js
@@ -1,5 +1,16 @@
 module.exports = {
     name: 'auth0',
+    schema: {
+        $id: 'auth0.json',
+        type: 'object',
+        properties: {
+            message: {
+                type: 'string',
+                description: 'Message to show',
+                examples: ['This is a log message']
+            }
+        },
+    },
     policy: (actionParams) => {
         return (req, res, next) => {
             console.log(req.headers)
